881790af7a49ae05be609ed1afa3fe72c2e96f45
Xlaoer <makjys@gmail.com>
2023-12-28 16:36:32 +0800

    分离Encoder和Decoder

--- module.py
+++ module.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Autoencoder(nn.Module):
+class Encoder(nn.Module):
     def __init__(self):
-        super(Autoencoder, self).__init__()
+        super(Encoder, self).__init__()
         # encoder
         self.enc1 = nn.Conv2d(
             in_channels=3, out_channels=8, kernel_size=3
@@ -12,6 +12,14 @@ class Autoencoder(nn.Module):
         self.enc2 = nn.Conv2d(
             in_channels=8, out_channels=4, kernel_size=3
         )
+    def forward(self, x):
+       x = F.relu(self.enc1(x))
+       x = F.relu(self.enc2(x))
+       return x
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
         # decoder
         self.dec1 = nn.ConvTranspose2d(
             in_channels=4, out_channels=8, kernel_size=3
@@ -20,8 +28,16 @@ class Autoencoder(nn.Module):
             in_channels=8, out_channels=3, kernel_size=3
         )
     def forward(self, x):
-       x = F.relu(self.enc1(x))
-       x = F.relu(self.enc2(x))
        x = F.relu(self.dec1(x))
        x = F.relu(self.dec2(x))
        return x
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+    def forward(self, x):
+       encoded_image = self.encoder(x)
+       decoded_image = self.decoder(encoded_image)
+       return decoded_image
